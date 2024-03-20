919c32d1a28149311007775b3e1b89829af46a3b
jjang051jhta <jjang051jhta@gmail.com>
2024-03-20 17:51:37 +0900

    practice02 8

--- practice-02/src/Practice08.java
+++ practice-02/src/Practice08.java
@@ -1,2 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Practice08 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("정수를 입력하세요");
+        int num = scanner.nextInt();
+        int arr[] = new int[num];
+        for(int i=0;i<num;i++) {
+            int randomInt;
+            boolean run;
+            do {
+                randomInt = (int) (Math.random() * 100) + 1;
+                run = false;
+                for (int j = 0; j < i; j++) {
+                    if (arr[j] == randomInt) {
+                        run = true;
+                        break;
+                    }
+                }
+            } while (run);
+            arr[i] = randomInt;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
 }
